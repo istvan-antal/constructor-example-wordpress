6f9db3cc2a818df209679d9ad56c6182a012febc
Antal István Miklós <istvan.m.antal@gmail.com>
2015-05-12 12:45:24 +0100

    Fixed .gitignore

---
+++ blueprint.py
@@ -0,0 +1,28 @@
+def build(infrastructure):
+    instance = infrastructure.create_instance(id=args.name)
+
+    instance.provision()
+    instance.setup_generic_php()
+
+    mysql = instance.create_mysql('Qtn46g6b')
+    mysql.create_db_with_user('wordpress', 'wordpress', 'wordpress')
+
+    instance.install_redis()
+    instance.use_redis_php_sessions()
+
+    instance.run_commands(
+        'mkdir -p /opt/apps',
+        'cd /opt/apps',
+        'wget https://wordpress.org/latest.tar.gz',
+        'tar xvzf latest.tar.gz',
+        'cd wordpress',
+        'mkdir wp-content/uploads',
+        'chmod -R 0777 wp-content'
+    )
+
+    instance.upload_file('wp-config.php', '/opt/apps/wordpress/wp-config.php')
+
+    instance.use_nginx_config('nginx.conf')
+    instance.install_my_key()
+
+    print "For mail sending please run: sudo apt-get install postfix"
